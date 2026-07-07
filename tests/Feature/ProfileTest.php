447265f52283749d4ff0150a9850c1599c493b5c
Amiral <amiralmoustaff25@gmail.com>
2026-07-07 13:19:30 +0000

    test: fix ProfileTest redirect assertions

--- tests/Feature/ProfileTest.php
+++ tests/Feature/ProfileTest.php
@@ -24,7 +24,7 @@ test('profile information can be updated', function () {
 
     $response
         ->assertSessionHasNoErrors()
-        ->assertRedirect('/profile');
+        ->assertRedirect('/profile/show');
 
     $user->refresh();
 
@@ -45,7 +45,7 @@ test('email verification status is unchanged when the email address is unchanged
 
     $response
         ->assertSessionHasNoErrors()
-        ->assertRedirect('/profile');
+        ->assertRedirect('/profile/show');
 
     $this->assertNotNull($user->refresh()->email_verified_at);
 });
